df970b1ae4e8fbdd64fde34a1d190ae768a46c61
Filipp Slabodetsky <oker211@gmail.com>
2021-01-28 16:32:45 +0300

    Initial Commit

---
+++ HomeWork 2.1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork 2.1.2
+//
+//  Created by Филипп Слабодецкий on 28.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
